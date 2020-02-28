6e3f208f79dc97081d6aeb30edf1859b5b17ee85
palmoreck <palmoreck@gmail.com>
2020-02-27 21:41:58 -0600

    adding numerical openblas for python

---
+++ jupyterlab/numerical_openblas/base/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+USER=miuser
+HOME=/home/${USER}
+cd ${HOME} && pip3 install --user cython==0.29.15 pybind11==2.4.3 pytest==5.3.5 && pip3 install --user numpy==1.18.1 --no-binary numpy #&& pip3 install scipy==1.4.1 --user --no-binary scipy
+
+cd / && /usr/local/bin/jupyter lab --ip=0.0.0.0 --no-browser
