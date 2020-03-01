dec93be5566ca687186c46b83927d83517cedd0a
palmoreck <palmoreck@gmail.com>
2020-03-01 12:07:51 -0600

    installation of R and IRkernel

--- jupyterlab/openblas/r_kernel/1.1.0/run.sh
+++ jupyterlab/openblas/r_kernel/1.1.0/run.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 USER=miuser
-HOME=/home/${USER}
-cd ${HOME} && pip3 install --user cython==0.29.15 pybind11==2.4.3 pytest==5.3.5 && \
-	      pip3 install --user numpy==1.18.1 --no-binary numpy && \
-	      pip3 install scipy==1.4.1 --user --no-binary scipy
-
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9 && \
+    sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/' && \
+    sudo apt-get update && \
+    sudo apt-get install -y r-base libssl-dev libxml2-dev libcurl4-openssl-dev
+sudo chmod gou+w /usr/local/lib/R/site-library && \
+    sudo R -e 'install.packages(c("repr", "IRdisplay", "evaluate", "crayon", "pbdZMQ", "devtools", "uuid", "digest"), lib="/usr/local/lib/R/site-library/")' && \
+    R -e 'devtools::install_github("IRkernel/IRkernel")' && \
+    sudo R -e 'IRkernel::installspec(user=FALSE)'
+sudo chmod gou+wx -R /home/miuser/.local
 cd / && /usr/local/bin/jupyter lab --ip=0.0.0.0 --no-browser
