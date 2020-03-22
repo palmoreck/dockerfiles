ff266e60f26f12ff02247655b14f18266e1996dd
erick <palmoreck@gmail.com>
2020-03-22 11:20:03 -0600

    moving installation of openblas to run

--- jupyterlab/openblas/r_kernel/1.1.0/run.sh
+++ jupyterlab/openblas/r_kernel/1.1.0/run.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 USER=miuser
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9 && \
-    sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/' && \
-    sudo apt-get update && \
-    sudo apt-get install -y r-base libssl-dev libxml2-dev libcurl4-openssl-dev
+sudo apt-get update && sudo apt-get install -y libopenblas-dev
+sudo apt-get update && \
+     sudo apt-get install -y r-base libssl-dev libxml2-dev libcurl4-openssl-dev
 sudo chmod gou+w /usr/local/lib/R/site-library && \
     sudo R -e 'install.packages(c("repr", "IRdisplay", "evaluate", "crayon", "pbdZMQ", "devtools", "uuid", "digest"), lib="/usr/local/lib/R/site-library/")' && \
     R -e 'devtools::install_github("IRkernel/IRkernel")' && \
