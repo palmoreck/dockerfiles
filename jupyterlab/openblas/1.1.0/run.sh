f8e6af2389af3d64b4b54b04886b78b553fbdcbc
erick <palmoreck@gmail.com>
2020-03-21 21:23:40 -0600

    moving installation of openblas to run.sh

--- jupyterlab/openblas/1.1.0/run.sh
+++ jupyterlab/openblas/1.1.0/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 USER=miuser
 HOME=/home/${USER}
+sudo apt-get update && sudo apt-get install -y libopenblas-dev
 cd ${HOME} && pip3 install --user cython==0.29.15 pybind11==2.4.3 pytest==5.3.5 && \
 	      pip3 install --user numpy==1.18.1 --no-binary numpy && \
 	      pip3 install scipy==1.4.1 --user --no-binary scipy
